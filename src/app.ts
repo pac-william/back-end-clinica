ac943fbbcc68d75fd2093acfb57ee7b0702a0b41
Victor Santos da Silva <santtosvictor42@gmail.com>
2025-05-12 17:14:50 -0300

    chore: not found router

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,13 @@ app.use('/api/v1/swagger-ui', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/', routes);
 
+app.use((req, res) => {
+    res.status(404).json({
+        message: 'Not Found',
+    });
+});
+
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}/api-docs`);
