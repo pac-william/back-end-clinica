d706b563a0aa4f32de8cef71f11ec3fdd1600e9f
romulogdonadoni <romulogdonadoni@gmail.com>
2025-05-25 02:19:23 -0300

    Add methods to QueryBuilder for processing number, string, and boolean values

--- src/utils/QueryBuilder.ts
+++ src/utils/QueryBuilder.ts
@@ -9,6 +9,13 @@ export class QueryBuilder {
         return new QueryBuilder(query);
     }
 
+    /**
+     * Processa um valor numérico da query
+     * 
+     * @param key Nome do parâmetro na query
+     * @param defaultValue Valor padrão a ser utilizado caso o parâmetro não exista ou seja inválido
+     * @returns Instância do QueryBuilder para encadeamento
+     */
     withNumber(key: string, defaultValue?: number): QueryBuilder {
         this.query[key] = this.query[key] !== undefined
             ? Number(this.query[key]) || defaultValue
@@ -16,8 +23,26 @@ export class QueryBuilder {
         return this;
     }
 
+    /**
+     * Processa um valor de string da query
+     * 
+     * @param key Nome do parâmetro na query
+     * @param defaultValue Valor padrão a ser utilizado caso o parâmetro não exista ou seja inválido
+     * @returns Instância do QueryBuilder para encadeamento
+     */
     withString(key: string, defaultValue?: string): QueryBuilder {
         this.query[key] = this.query[key] !== undefined ? String(this.query[key]) : defaultValue;
+        return this;
+    }
+
+    /**
+     * Processa um valor booleano da query
+     * 
+     * @param key Nome do parâmetro na query
+     * @param defaultValue Valor padrão a ser utilizado caso o parâmetro não exista ou seja inválido
+     */
+    withBoolean(key: string, defaultValue?: boolean): QueryBuilder {
+        this.query[key] = this.query[key] !== undefined ? this.query[key] === 'true' : defaultValue;
         return this;
     }
 
