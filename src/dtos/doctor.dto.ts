43301a410d101d7ee7a9d038da8b61511e8edddd
Victor Santos da Silva <santtosvictor42@gmail.com>
2025-05-26 20:22:11 -0300

    fix(doctorDTO): export DoctorDTO for external usage

--- src/dtos/doctor.dto.ts
+++ src/dtos/doctor.dto.ts
@@ -1,11 +1,9 @@
 import { z } from 'zod';
 
-const DoctorDTO = z.object({
+export const DoctorDTO = z.object({
   name: z.string().min(3, 'Nome deve ter pelo menos 3 caracteres'),
   crm: z.string().min(4, 'CRM deve ter pelo menos 4 caracteres'),
   specialty: z.string().min(3, 'Especialidade deve ter pelo menos 3 caracteres'),
   phone: z.string(),
   email: z.string().email('Email inválido')
 });
-
-export default DoctorDTO;
