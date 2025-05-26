e73630dac2d7cc80c274a221fc986e26893bdada
Victor Santos da Silva <santtosvictor42@gmail.com>
2025-05-26 20:58:49 -0300

    fix(doctorDTO): corrigir nome da propriedade 'specialty' para 'specialties'

--- src/dtos/doctor.dto.ts
+++ src/dtos/doctor.dto.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const DoctorDTO = z.object({
   name: z.string().min(3, 'Nome deve ter pelo menos 3 caracteres'),
   crm: z.string().min(4, 'CRM deve ter pelo menos 4 caracteres'),
-  specialty: z.string().min(3, 'Especialidade deve ter pelo menos 3 caracteres'),
+  specialties: z.string().min(3, 'Especialidade deve ter pelo menos 3 caracteres'),
   phone: z.string(),
   email: z.string().email('Email inválido')
 });
