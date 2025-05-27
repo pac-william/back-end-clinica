fe90d3d6ac0f576039522bc4c42c6fc12a12316b
Victor Santos da Silva <santtosvictor42@gmail.com>
2025-05-26 21:22:12 -0300

    fix(utils): change to english

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,11 +5,11 @@ class Utils
         if(cpf.toString().length != 11 || /^(\d)\1{10}$/.test(cpf)) return false;
         var result = true;
         [9,10].forEach(function(j){
-            var soma = 0, r;
+            var sum = 0, r;
             cpf.split(/(?=)/).splice(0,j).forEach(function(e, i){
-                soma += parseInt(e) * ((j+2)-(i+1));
+                sum += parseInt(e) * ((j+2)-(i+1));
             });
-            r = soma % 11;
+            r = sum % 11;
             r = (r <2)?0:11-r;
             if(r != parseInt(cpf.substring(j, j+1))) result = false;
         });
