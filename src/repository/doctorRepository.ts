44cbceed2894518bc69b4314d6290aeda4e3d98b
romulogdonadoni <romulogdonadoni@gmail.com>
2025-05-26 23:29:25 -0300

    fix(doctorRepository): refactor getAllDoctors method to improve specialty filtering and enhance doctor response structure

--- src/repository/doctorRepository.ts
+++ src/repository/doctorRepository.ts
@@ -4,24 +4,38 @@ import db from "../database/connection";
 export class DoctorRepository {
     async getAllDoctors(page: number, size: number, specialty?: number[], name?: string): Promise<DoctorPaginatedResponse> {
         const offset = (page - 1) * size;
-        let query = db('doctors');
+        let queryDoctor = db('doctors');
+        let querySpecialty = db('specialty_doctor');
 
         if (specialty && specialty.length > 0) {
             const specialtyIds = specialty.map(Number);
-            query = query.whereRaw('specialties_ids && ?::int[]', [specialtyIds]);
+            querySpecialty = querySpecialty.whereIn('specialty_id', specialtyIds);
+            
+            const doctorIds = db('specialty_doctor')
+                .whereIn('specialty_id', specialtyIds)
+                .select('doctor_id');
+                
+            queryDoctor = queryDoctor.whereIn('id', doctorIds);
         }
 
         if (name) {
-            query = query.whereRaw('LOWER(name) LIKE LOWER(?)', [`%${name}%`]);
+            queryDoctor = queryDoctor.whereRaw('LOWER(name) LIKE LOWER(?)', [`%${name}%`]);
         }
 
-        const countResult = await query.clone().count('id as count').first();
+        const countResult = await queryDoctor.clone().count('id as count').first();
         const total = countResult ? Number(countResult.count) : 0;
 
-        const doctors = await query.offset(offset).limit(size);
-
+        const doctors = await queryDoctor.offset(offset).limit(size);
+        const specialties = await db('specialty_doctor')
+            .join('specialties', 'specialties.id', 'specialty_doctor.specialty_id')
+            .whereIn('doctor_id', doctors.map(doctor => doctor.id))
+            .select('specialty_doctor.doctor_id', 'specialty_doctor.specialty_id', 'specialties.name as specialty_name');
+            
         return {
-            doctors: doctors,
+            doctors: doctors.map(doctor => ({
+                ...doctor,
+                specialties: specialties.filter(specialty => specialty.doctor_id === doctor.id).map(specialty => specialty.specialty_id)
+            })),
             meta: {
                 total: total,
                 page: page,
