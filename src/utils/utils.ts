a6809088e46ebabe5bfc7e4b5cafb0e253c74d24
romulogdonadoni <romulogdonadoni@gmail.com>
2025-05-29 03:02:35 -0300

    refactor(Utils): improve CPF validation logic and add formatPhone method

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,23 +1,28 @@
-class Utils 
-{
-    public static checkCPF(cpf: string){
+class Utils {
+    public static checkCPF(cpf: string) {
         cpf = cpf.replace(/\D/g, '');
-        if(cpf.toString().length != 11 || /^(\d)\1{10}$/.test(cpf)) return false;
+        if (cpf.toString().length != 11 || /^(\d)\1{10}$/.test(cpf)) return false;
         var result = true;
-        [9,10].forEach(function(j){
+        [9, 10].forEach(function (j) {
             var sum = 0, r;
-            cpf.split(/(?=)/).splice(0,j).forEach(function(e, i){
-                sum += parseInt(e) * ((j+2)-(i+1));
+            cpf.split(/(?=)/).splice(0, j).forEach(function (e, i) {
+                sum += parseInt(e) * ((j + 2) - (i + 1));
             });
             r = sum % 11;
-            r = (r <2)?0:11-r;
-            if(r != parseInt(cpf.substring(j, j+1))) result = false;
+            r = (r < 2) ? 0 : 11 - r;
+            if (r != parseInt(cpf.substring(j, j + 1))) result = false;
         });
         return result;
     }
 
     public static removeMask(value: string): string {
         return value.replace(/\D/g, '');
+    }
+
+    public static formatPhone(phone: string | undefined | null): string {
+        if (!phone) return '';
+        const numericOnly = this.removeMask(phone);
+        return numericOnly.trim().substring(0, 15);
     }
 }
 
